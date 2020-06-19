e06cc44e286a5bcde18f5bf420d71e2f8fcb5e5e
Rob Bayliss <rob@lastcallmedia.com>
2020-06-19 10:34:33 -0400

    Make smartform smarter

--- src/SmartForm.tsx
+++ src/SmartForm.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import { Schema, SchemaHandler } from "./types";
+import { SchemaHandler, SchemaFromSchemaHandler } from "./types";
 
-type SmartFormProps = {
-  handler: SchemaHandler;
-  schema: Schema;
-};
-export default function SmartForm({
-  handler,
-  schema,
-}: SmartFormProps): React.ReactElement {
+export default function SmartForm<
+  H extends SchemaHandler,
+  S extends SchemaFromSchemaHandler<H>
+>({ handler, schema }: { handler: H; schema: S }): React.ReactElement {
   const formContext = useForm();
-
   return <form>{handler.getReactElement(schema, { form: formContext })}</form>;
 }
