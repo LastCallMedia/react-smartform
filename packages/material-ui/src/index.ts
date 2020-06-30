d7cb4f52253a13fc01fddaf1b734d5a4746163d9
Rob Bayliss <rob@lastcallmedia.com>
2020-06-30 10:43:40 -0400

    Fix typing

--- packages/material-ui/src/index.ts
+++ packages/material-ui/src/index.ts
@@ -2,7 +2,6 @@ import * as handlerObj from "./fields";
 import { Registry, registry as coreRegistry } from "@lastcall/react-smartform";
 
 const materialRegistry = new Registry(
-  Object.values(handlerObj).map((cls) => new cls()),
-  {}
+  Object.values(handlerObj).map((cls) => new cls())
 );
 export const registry = coreRegistry.merge(materialRegistry);
